c0a641aab6b686f2d71207d68d74528dda01cda8
tianyu <zhangtianyu5177@sina.com>
2019-08-17 13:12:06 +0800

    update

--- code/04_AMD-RequireJS/js/modules/dataService.js
+++ code/04_AMD-RequireJS/js/modules/dataService.js
@@ -1,15 +1,3 @@
 /*
 * 定义一个没有依赖的模块
 * */
-
-define(function () {
-
-  let msg = 'hello!atguigu'
-
-  function getMsg() {
-    return msg.toUpperCase()
-  }
-
-  return {getMsg}
-
-})
--- code/04_AMD-RequireJS/js/modules/loger.js
+++ code/04_AMD-RequireJS/js/modules/loger.js
@@ -2,13 +2,3 @@
 * 定义一个有依赖的模块
 * */
 
-define(['dataService'],function (dataService) {
-  let name = 'tom'
-
-  function speak() {
-    console.log(`${name}说${dataService.getMsg()}`)
-  }
-
-  return {speak}
-
-})
